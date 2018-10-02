9dc594caad9e805227201322b8731c609513dce3
Abigail Lu <abigailitliong.lu@calbaptist.edu>
2018-10-01 23:16:36 -0700

    working on setting up logger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,36 @@
+import Threads.myThread;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Name: Abigail Lu
+ * Date: 10/1/2018
+ * Threads Homework
+ * CSC 527
+ *
+ * Description:
+ * I would like to know how many UFO sightings occur in each of the following:
+
+     Country
+     State, (If within the US/Canada)
+     Occur during the following times:
+         8:01-12:00
+         12:01-16:00
+         16:01-20:00
+         20:01-0:00
+         0:01-8:00
+     Month
+     Year
+ */
 public class Main {
+
+    public static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        logger.info("Hello, world");
+//        myThread main = new myThread(10);
+//        main.main();
+
     }
 }
--- src/main/java/Threads/myThread.java
+++ src/main/java/Threads/myThread.java
@@ -16,13 +16,8 @@ public class myThread {
 
     // Hold all our threads.
     private List<Thread> threads = new ArrayList<Thread>();
-//    private List<NumberMapper> numberMappers = new ArrayList<NumberMapper>();
 
-    private Map<Integer, List<Integer>> intMap = new HashMap<Integer, List<Integer>>();
-
-//    public static final Logger logger = LogManager.getLogger(ThreadMain.class);
-
-    private File inputFile = new File("ufo/main/resources/ufo-sightings/scrubbed.csv");
+    public static final Logger logger = LogManager.getLogger(myThread.class);
 
     public myThread(int threads) {
         this.numThreads = threads;
@@ -30,62 +25,55 @@ public class myThread {
 
     public void main() {
 
-        FileInputStream fis;
-        BufferedReader bis;
+        String csvFile = "ufo/main/resources/ufo-sightings/scrubbed.csv";
+        String line = "";
+        String cvsSplitBy = ",";
 
-        try {
-            fis = new FileInputStream(this.inputFile);
-            bis = new BufferedReader(new InputStreamReader(fis));
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 
-            String line = bis.readLine();
+            while ((line = br.readLine()) != null) {
 
-            int counter = 0;
-            while (line != null) {
-                if (!StringUtils.isEmpty(line)) {
-                    if (!this.intMap.containsKey(counter % this.numThreads)) {
-                        this.intMap.put(counter % this.numThreads, new ArrayList<Integer>());
-                    }
-                    this.intMap.get(counter % this.numThreads).add(Integer.parseInt(StringUtils.strip(line)));
-                    counter++;
-                }
-                line = bis.readLine();
+                // use comma as separator
+                String[] ufoInfo = line.split(cvsSplitBy);
+
+                logger.info("DateTime [code= " + ufoInfo[0] + " , City=" + ufoInfo[1] + "]");
+
             }
-            bis.close();
+
         } catch (IOException e) {
-//            logger.fatal(e.toString());
             e.printStackTrace();
         }
 
-        for (int x = 0; x < this.numThreads; x++) {
-//            NumberMapper nm = new NumberMapper(this.intMap.get(x));
-//            this.numberMappers.add(nm);
-//            Thread thread = new Thread(nm);
-//            this.threads.add(thread);
-        }
-
-        int x = 1;
-        for (Thread thread : this.threads) {
-//            logger.debug("Running thread: " + x);
-            thread.start();
-            x++;
-        }
-
-        int isDone = numThreads;
-
-        while (isDone > 0) {
-//            for (NumberMapper thread : this.numberMappers) {
-//                if (thread.isDone()) {
-//                    isDone--;
-//                }
-//            }
-        }
-
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (Exception e) {
-        }
-
-//        logger.info(NumberMapper.mapper);
+//        for (int x = 0; x < this.numThreads; x++) {
+////            NumberMapper nm = new NumberMapper(this.intMap.get(x));
+////            this.numberMappers.add(nm);
+////            Thread thread = new Thread(nm);
+////            this.threads.add(thread);
+//        }
+//
+//        int x = 1;
+//        for (Thread thread : this.threads) {
+////            logger.debug("Running thread: " + x);
+//            thread.start();
+//            x++;
+//        }
+//
+//        int isDone = numThreads;
+//
+//        while (isDone > 0) {
+////            for (NumberMapper thread : this.numberMappers) {
+////                if (thread.isDone()) {
+////                    isDone--;
+////                }
+////            }
+//        }
+//
+//        try {
+//            TimeUnit.SECONDS.sleep(2);
+//        } catch (Exception e) {
+//        }
+//
+////        logger.info(NumberMapper.mapper);
 
 
     }
